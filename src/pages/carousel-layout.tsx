bb16b4f36e079000dd768e5dab018d8a897155e3
D270RG <d270rg@gmail.com>
2022-08-02 21:12:38 +0300

    Carousel adaptivity

--- src/pages/carousel-layout.tsx
+++ src/pages/carousel-layout.tsx
@@ -10,12 +10,23 @@ import SocialWidget from '../widgets/SocialWidget/SocialWidget';
 import {ThreeDots} from 'react-bootstrap-icons';
 import Texts from './texts';
 const panorama = require('./pictures/panorama.jpg');
-
+var contentArr = [
+    <div className='w-100 h-100' style={{backgroundImage: 'url(' + require('./pictures/city1.jpg') + ')',borderRadius:'20px'}}>1</div>,
+    <div className='w-100 h-100' style={{backgroundImage: 'url(' + require('./pictures/city2.jpg') + ')',borderRadius:'20px'}}>2</div>,
+    <div className='w-100 h-100' style={{backgroundImage: 'url(' + require('./pictures/city3.jpg') + ')',borderRadius:'20px'}}>3</div>,
+    <div className='w-100 h-100' style={{backgroundImage: 'url(' + require('./pictures/city4.jpg') + ')',borderRadius:'20px'}}>4</div>,
+    <div className='w-100 h-100' style={{backgroundImage: 'url(' + require('./pictures/city5.jpg') + ')',borderRadius:'20px'}}>5</div>,
+    <div className='w-100 h-100' style={{backgroundImage: 'url(' + require('./pictures/city6.jpg') + ')',borderRadius:'20px'}}>6</div>,
+    <div className='w-100 h-100' style={{backgroundImage: 'url(' + require('./pictures/city7.jpg') + ')',borderRadius:'20px'}}>7</div>,
+    <div className='w-100 h-100' style={{backgroundImage: 'url(' + require('./pictures/city8.jpg') + ')',borderRadius:'20px'}}>8</div>,
+    <div className='w-100 h-100' style={{backgroundImage: 'url(' + require('./pictures/city9.jpg') + ')',borderRadius:'20px'}}>9</div>,
+    <div className='w-100 h-100' style={{backgroundImage: 'url(' + require('./pictures/city10.jpg') + ')',borderRadius:'20px'}}>10</div>,
+  ]
 function Page(){
     return(
-        <Container fluid style={{backgroundImage: 'url("http://via.placeholder.com/256x256")'}}>
+        <Container fluid className='m-0 p-0' style={{backgroundImage: 'url("http://via.placeholder.com/256x256")'}}>
             <Row className='d-xs' style={{height:'calc(100vh*0.03)'}}>{/*space*/}</Row>
-                <CarouselWidget scale={true} shrink={false}/>
+                <CarouselWidget scale={true} width={200} contentArr={contentArr}/>
             <Row className='d-xs' style={{height:'calc(100vh*0.03)'}}>{/*space*/}</Row>
         </Container>
     );
--- src/widgets/CarouselWidget/CarouselWidget.tsx
+++ src/widgets/CarouselWidget/CarouselWidget.tsx
@@ -1,31 +1,11 @@
 import {useState,useEffect,MouseEventHandler} from 'react';
-import { Col,Row,Ratio,Container} from 'react-bootstrap';
-import './CarouselWidget.css';
+import { Col,Row,Ratio,Container,Button} from 'react-bootstrap';
 function sumMaps(a:Map<string,number>,b:Map<string,number>){
   a.forEach((value,key)=>{
     a.set(key,value+(b.get(key) as number));
   });
   return a;
 };
-class CycledList{
-  contentArr:JSX.Element[];
-  constructor(){
-    this.contentArr = [
-      <div>1</div>,
-      <div>2</div>,
-      <div>3</div>,
-      <div>4</div>,
-      <div>5</div>,
-      <div>6</div>,
-      <div>7</div>,
-      <div>4</div>,
-      <div>5</div>,
-      <div>6</div>,
-  
-    ]
-  }
-  
-}
 class TransitionMap{
   val:Map<string,number>;
   constructor(values?:Map<string,number>,other?:TransitionMap){
@@ -34,7 +14,7 @@ class TransitionMap{
     } else if(other!==undefined){
       this.val = other.get();
     } else {
-      this.val = new Map([['px',0],['%',-150],['rem',0]]);
+      this.val = new Map([['px',0],['%',0],['rem',0]]); //TODO: initial calc initial(contentArr.len,container.len(contentArr.len))
     }
   }
   formString(){
@@ -48,29 +28,45 @@ class TransitionMap{
     return this.val;
   }
 }
+
+function InteractiveButton(props){ //I don't want to declare types here
+  //TODO: declare props as states
+  var [index,setIndex] = useState(props.index);
+  function move(index:number){
+    console.log('callback', index);
+    props.setTransition(new TransitionMap(sumMaps(props.transition.get(),new Map([['px',0],['%',(props.center-index)*100],['rem',0]]))));
+    props.setCenter(index);
+  }
+  var style = {
+    backgroundColor: 'transparent',
+    border: 'none',
+    transform: props.transitionString+props.initialShiftString,
+    filter: 'contrast('+props.opacityAmount+')',
+    transition: '0.5s linear',
+    transitionDuration: '1s',
+    zIndex: 999-props.distance,
+    width: props.width+'px',
+  }
+  if(props.doScale === true){
+    style.transform += props.scaleString;
+  }
+  if(props.doShrink === true){
+    style.transform += props.shrinkString;
+  }
+  return(<button style={style} className='m-0 h-100 p-0 m-0' onClick={()=>{move(index)}}>{props.child}</button>);
+}
+
 function Carousel(props){
   var scaleCoeff = 0.75; //coeff+ => bigger elements, default 0.5
-  var shrinkCoeff = 2; //shrink+ => more shrink, default 2
-  var contentArr = [
-    <div>1</div>,
-    <div>2</div>,
-    <div>3</div>,
-    <div>4</div>,
-    <div>5</div>,
-    <div>6</div>,
-    <div>7</div>,
-    <div>8</div>,
-    <div>9</div>,
-    <div>10</div>,
+  var shrinkCoeff = 0.5; //shrink+ => more shrink, default 2
 
-  ]
   var [transition,setTransition] = useState(new TransitionMap());
-  var [center,setCenter] = useState(Math.floor((contentArr.length)/2));
+  var [center,setCenter] = useState(Math.floor((props.contentArr.length)/2));
   var transitionString:string = transition.formString();
 
   function formStyles(){
     var tileArr:JSX.Element[] = [];
-    contentArr.forEach((frame,index)=>{
+    props.contentArr.forEach((frame,index)=>{
       var centricity = index-center;
       var distance = Math.abs(centricity)+1;
 
@@ -82,40 +78,33 @@ function Carousel(props){
       var shrinkModifier = -Math.sign(centricity)*Math.pow(centricity,2)*shrinkCoeff;
       var shrinkString = ' translateX('+shrinkModifier+'rem)'
 
-      var style = {
-        transform: transitionString,
-        filter: 'contrast('+opacityAmount+')',
-        transition: '1s linear',
-        transitionDuration: '1s',
-        zIndex: 9999-distance,
-        width: '8%',
-      }
-      if(props.scale === true){
-        style.transform += scaleString;
-      }
-      if(props.shrink === true){
-        style.transform +=shrinkString;
-      }
-      tileArr.push(<Container style={style} className='h-100 debug mx-1 element p-0'>{frame}</Container>);
+      var initialShift = (props.contentArr.length*(props.width))/2+props.width/2;
+      var initialShiftString = ' translateX(calc(50vw - '+initialShift+'px))';
+
+      tileArr.push(<InteractiveButton child={frame}
+                  setCenter={setCenter} setTransition = {setTransition} index={index}
+                  transitionString={transitionString} initialShiftString={initialShiftString} shrinkString={shrinkString} scaleString={scaleString}
+                  opacityAmount={opacityAmount} distance={distance} width={props.width} transition={transition} center={center}
+                  doShrink={props.shrink} doScale={props.scale}></InteractiveButton>);
     });
     return tileArr;
   }
   
   return(
-    <Container fluid>
-      <Row className='debug' style={{height:'400px',width:'150%',overflow:'hidden'}}>
+    <Container className='m-0 p-0' fluid style={{overflow:'hidden'}}>
+      <Row className='carousel-row m-0 p-0' style={{height:'400px',width:'1000%',overflow:'hidden'}}>
           {formStyles()}
       </Row>
       <Row>
           <button className='btn h-15 w-15' onClick={()=>{
                                       if(center>0){
-                                        setTransition(new TransitionMap(sumMaps(transition.get(),new Map([['px',2],['%',100],['rem',0.25]]))));
+                                        setTransition(new TransitionMap(sumMaps(transition.get(),new Map([['px',0],['%',100],['rem',0]]))));
                                         setCenter(center-1);
                                       }
                                       }}>Right</button>
           <button className='btn h-15 w-15' onClick={()=>{
-                                    if(center<contentArr.length-1){
-                                      setTransition(new TransitionMap(sumMaps(transition.get(),new Map([['px',-2],['%',-100],['rem',-0.25]]))));
+                                    if(center<props.contentArr.length-1){
+                                      setTransition(new TransitionMap(sumMaps(transition.get(),new Map([['px',0],['%',-100],['rem',-0]]))));
                                       setCenter(center+1);
                                     }
                                       }}>Left</button>
